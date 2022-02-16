8e62033637c4fd10b6b9053492ac79ff0077e1b3
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-16 20:42:28 +0530

    Alice 🤍🥀

--- plugins/pinterestvideo.js
+++ plugins/pinterestvideo.js
@@ -2,7 +2,7 @@ const { pin } = require('../lib/scrape')
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-    if (!args[0]) throw `uhm.. url nya mana?\n\ncontoh:\n${usedPrefix + command} https://id.pinterest.com/pin/267893877823775677/`
+    if (!args[0]) throw `uhm.. where's the url?\n\nExample:\n${usedPrefix + command} https://id.pinterest.com/pin/582864376776055837/`
     if (!args[0].match(/https:\/\/.*pinterest.com\/pin|pin.it/gi)) throw `url salah`
 
     pin(args[0]).then(async res => {
