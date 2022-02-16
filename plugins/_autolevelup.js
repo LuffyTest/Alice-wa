ed9d10c94e17398e729520a8d61eb62be840dffc
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-16 12:03:56 +0530

    Alice 🤍🥀

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -34,8 +34,8 @@ handler.before = async function (m) {
                 }, 'apikey')
                 let res = await fetch(url)
                 if (before !== user.level) {
-                        if (!res.ok) return this.sendButton(m.chat, `*Naik Level!*\n*${before}* ➞ *${user.level}*`, '© stikerin', 'Ambil XP Harian', ',daily', m)
-                        await this.sendButtonImg(m.chat, url, `*Naik Level!*\n*${before}* ➞ *${user.level}*`, '© stikerin', 'Ambil XP Harian', ',daily', m)
+                        if (!res.ok) return this.sendButton(m.chat, `*Naik Level!*\n*${before}* ➞ *${user.level}*`, '© Alice 🤍🥀', 'Take Daily XP', ',daily', m)
+                        await this.sendButtonImg(m.chat, url, `*Naik Level!*\n*${before}* ➞ *${user.level}*`, '© Alice 🤍🥀', 'Take Daily XP', ',daily', m)
                 }
         }
 }
