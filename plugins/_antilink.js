26c081cfe82e3a5dbae3a4d225370015d2477093
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-16 12:01:19 +0530

    Alice 🤍🥀

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -9,7 +9,7 @@ handler.before = async function (m, { isAdmin, isBotAdmin }) {
   if (chat.antiLink && isGroupLink && !isAdmin && !m.isBaileys && m.isGroup) {
     let thisGroup = `https://chat.whatsapp.com/${await conn.groupInviteCode(m.chat)}`
     if (m.text.includes(thisGroup)) throw !0 // jika link grup itu sendiri gak dikick
-    await this.sendButon(m.chat, `*GROUP LINK DETECTED!!*${isBotAdmin ? '' : '\n\nNot admin so cant kick t_t'}\n\nType *.off antilink* to turn off this feature${db.data.settings[this.user.jid].restrict ? '' : '\ntype *.on restrict* so you can kick'}`, '© Rias Gremory', 'Turn off Antilink', ',0 antilink', m)
+    await this.sendButon(m.chat, `*GROUP LINK DETECTED!!*${isBotAdmin ? '' : '\n\nNot admin so cant kick t_t'}\n\nType *.off antilink* to turn off this feature${db.data.settings[this.user.jid].restrict ? '' : '\ntype *.on restrict* so you can kick'}`, '© Alice 🤍🥀', 'Turn off Antilink', ',0 antilink', m)
     if (db.data.settings[this.user.jid].restrict) {
       if (isBotAdmin) this.groupRemove(m.chat, [m.sender])
     }
