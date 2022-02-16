486e9fdd8e10043ca233deab22b06cc2bfa06936
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-16 12:29:19 +0530

    Update asupan.js

--- plugins/asupan.js
+++ plugins/asupan.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-  conn.sendFile(m.chat, API('mel', '/asupan', {}, 'apikey'), 'asupan.mp4', '© melcanz x ariffb')
+  conn.sendFile(m.chat, API('mel', '/asupan', {}, 'apikey'), 'asupan.mp4', '© Alice 🤍🥀')
 }
 handler.help = ['asupan']
 handler.tags = ['fun']
