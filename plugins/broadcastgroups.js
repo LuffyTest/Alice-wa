31b4f04f78481a03e80dafb206d9ffabe2af35a7
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-16 20:28:32 +0530

    🥀

--- plugins/broadcastgroups.js
+++ plugins/broadcastgroups.js
@@ -2,14 +2,14 @@ let handler = async (m, { conn, text }) => {
   let groups = conn.chats.all().filter(v => v.jid.endsWith('g.us')).map(v => v.jid)
   let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
   let teks = text ? text : cc.text
-  conn.reply(m.chat, `Mengirim pesan broadcast ke ${groups.length} grup\nEstimasi selesai ${groups.length * 1.5} detik`, m)
+  conn.reply(m.chat, `Sends a broadcast message to ${groups.length} groups\nEstimated completion ${groups.length * 1.5} seconds`, m)
   for (let id of groups) {
     await conn.delay(1500)
-    await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : '「 *Stikerin Broadcast* 」\n\n' + teks + '\n\n© stikerin'), true).catch(_ => _)
+    await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : '「 *Alice Broadcast -🥀* 」\n\n' + teks + '\n\n© Alice 🥀'), true).catch(_ => _)
   }
-  m.reply('*Broadcast Selesai*')
+  m.reply('*Broadcast Complete*')
 }
-handler.help = ['broadcastgroup', 'bcgc'].map(v => v + ' <teks>')
+handler.help = ['broadcastgroup', 'bcgc'].map(v => v + ' <text>')
 handler.tags = ['owner']
 handler.command = /^(broadcast|bc)(group|grup|gc)$/i
 
