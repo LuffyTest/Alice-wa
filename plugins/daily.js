c0a9d276acbd7b0ea481ea3477a623f7bd0a3acf
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-16 12:38:48 +0530

    Alice 🤍🥀

--- plugins/daily.js
+++ plugins/daily.js
@@ -2,11 +2,11 @@ const free = 500
 const prem = 5000
 
 let handler = async (m, { conn, usedPrefix, isPrems }) => {
-  if (db.data.users[m.sender].level < 1) return await conn.sendButton(m.chat, 'Kamu masih level 0', '© stikerin', 'Naikan Level', `${usedPrefix}levelup`, m)
+  if (db.data.users[m.sender].level < 1) return await conn.sendButton(m.chat, 'You are still level 0', '© Alice 🤍🥀', 'Level Up', `${usedPrefix}levelup`, m)
   let time = db.data.users[m.sender].lastclaim + 86400000
   if (new Date - db.data.users[m.sender].lastclaim < 86400000) throw `Kamu sudah mengklaim klaim harian hari ini\nTunggu selama ${conn.msToTime(time - new Date())} lagi`
   db.data.users[m.sender].exp += isPrems ? prem * db.data.users[m.sender].level : free * db.data.users[m.sender].level
-  m.reply(`+${isPrems ? prem * db.data.users[m.sender].level : free * db.data.users[m.sender].level} XP\n\nSemakin tinggi level, semakin tinggi juga XP yang didapat`)
+  m.reply(`+${isPrems ? prem * db.data.users[m.sender].level : free * db.data.users[m.sender].level} XP\n\nThe higher the level, the higher the XP you get`)
   db.data.users[m.sender].lastclaim = new Date * 1
 }
 handler.help = ['daily', 'claim']
