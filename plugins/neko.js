3dd73b388e79711f136e483ede79f70e8e7ca3b0
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-16 13:13:59 +0530

    Update neko.js

--- plugins/neko.js
+++ plugins/neko.js
@@ -3,11 +3,11 @@ let handler = async (m, { conn }) => {
   let res = await fetch('https://api.waifu.pics/sfw/neko')
   if (!res.ok) throw eror
   let json = await res.json()
-  if (!json.url) throw 'Eror!'
-  conn.sendFile(m.chat, json.url, '', '© stikerin', m, 0, { thumbnail: await (await fetch(json.url)).buffer() })
+  if (!json.url) throw 'Error!'
+  conn.sendFile(m.chat, json.url, '', '© Alice 🤍🥀', m, 0, { thumbnail: await (await fetch(json.url)).buffer() })
 }
 handler.help = ['neko']
-handler.tags = ['internet']
+handler.tags = ['anime']
 handler.command = /^neko$/i
 
 module.exports = handler
