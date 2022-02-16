5332fb2f5414319db5bb3052e7a72705dea5ecfc
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-16 20:26:18 +0530

    Alice 🤍🥀

--- plugins/anonymous_sendkontak.js
+++ plugins/anonymous_sendkontak.js
@@ -12,16 +12,16 @@ async function handler(m, { command, conn, text }) {
 	this.anonymous = this.anonymous ? this.anonymous : {}
 	let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 	let room = Object.values(this.anonymous).find(room => room.check(who))
-	if (!room) throw 'kamu tidak berada di anonymous chat'
+	if (!room) throw 'you are not in anonymous chat'
 	let other = room.other(who)
   var name
   if (text) name = text
   else name = conn.getName(m.sender)
 	var number = who.split('@')[0]
-	if (other) this.sendMessage(other, `Partner mengirimkan kontak kepadamu`, MessageType.text)
+	if (other) this.sendMessage(other, `artner sent you contact`, MessageType.text)
 	if (other) this.sendContact(other, number, name, m)
 }
-handler.help = ['sendkontak']
+handler.help = ['sendcontact']
 handler.tags = 'anonymous'
 handler.command = /^(sendkontak)$/i
 handler.private = true
