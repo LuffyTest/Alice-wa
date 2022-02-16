0834fc594a7355e92f067ba6dd483cd17b48b7b2
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-16 12:32:01 +0530

    Update addprem.js

--- plugins/addprem.js
+++ plugins/addprem.js
@@ -1,23 +1,23 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-    let who
-    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
-    else who = m.chat
-    let user = db.data.users[who]
-    if (!who) return m.reply(`Tag/Mention!\n\nContoh:\n${usedPrefix + command} @0 1\n\nAngka 1 menunjukan total hari`)
-    let txt = text.replace('@' + who.split`@`[0], '').trim()
-    if (!txt) throw `Angkanya mana?\n\nContoh:\n${usedPrefix + command} @0 1`
-    if (isNaN(txt)) return m.reply(`Hanya angka!\n\nContoh:\n${usedPrefix + command} @0 1`)
-    var jumlahHari = 86400000 * txt
-    var now = new Date() * 1
-    if (now < user.premiumTime) user.premiumTime += jumlahHari
-    else user.premiumTime = now + jumlahHari
-    user.premium = true
-    m.reply(`Berhasil menambahkan *${user.name}* sebagai pengguna Premium selama ${txt} hari.\n\nHitung mundur: ${conn.msToDate(user.premiumTime - now)}`)
-}
-handler.help = ['addprem [@user] <angka>']
-handler.tags = ['owner']
-handler.command = /^(add|tambah|\+)p(rem)?$/i
+let fs = require('fs')
+let handler = async (m, { conn, text }) => {
 
-handler.rowner = true
+    const json = JSON.parse(fs.readFileSync('./src/premium.json'))
+    let who
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '') + '@s.whatsapp.net'
+    else who = text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
+    if (json.includes(who.split`@`[0])) throw `${conn.getName(who)} already premium!`
+    json.push(`${who.split`@`[0]}`)
+    fs.writeFileSync('./src/premium.json', JSON.stringify(json))
+    m.reply(`${conn.getName(who)} now premium!`)
+
+    delete require.cache[require.resolve('../config')]
+    require('../config')
+
+}
+handler.help = ['addprem [@user]']
+handler.tags = ['owner']
+handler.command = /^(add|plus|\+)prem$/i
+
+handler.owner = true
 
 module.exports = handler
