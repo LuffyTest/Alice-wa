3f8d261990f39705ccdfc6caf24df6b7f7fa9354
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-17 10:36:24 +0530

    alice

--- plugins/wikipedia.js
+++ plugins/wikipedia.js
@@ -1,14 +1,14 @@
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `Pengunaan:\n${usedPrefix + command} <teks>\n\nContoh:\n${usedPrefix + command} Javascript`
+  if (!text) throw `Use:\n${usedPrefix + command} <teks>\n\nExample:\n${usedPrefix + command} Javascript`
   let res = await fetch(API('amel', '/wikipedia', { q: text }, 'apikey'))
   if (!res.ok) throw eror
   let json = await res.json()
   if (!json.status) throw json
   m.reply(json.isi)
 }
-handler.help = ['wikipedia <teks>']
+handler.help = ['wikipedia <text>']
 handler.tags = ['internet']
 handler.command = /^(wiki(pedia)?)$/i
 
