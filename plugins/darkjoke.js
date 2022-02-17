ecf89fa77162ffb34e509c025066cebd4e5a59dc
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-17 19:03:26 +0530

    Update darkjoke.js

--- plugins/darkjoke.js
+++ plugins/darkjoke.js
@@ -1,8 +1,20 @@
-let handler = async (m, { conn }) => {
-    conn.sendFile(m.chat, global.API('joke', '/joke/Dark', {},), '', '', m, 0, { thumbnail: Buffer.alloc(0) })
+const fetch = require('node-fetch')
+
+let handler = async (m, { text, usedPrefix, command }) => {
+    let res = await fetch(API('https://v2.jokeapi.dev/', '/joke/dark', {
+        q: text,
+    }))
+    if (!res.ok) throw eror
+    let json = await res.json()
+    if (json.cod != 200) throw json
+    m.reply(`
+🎗 Category: ${response.data.category}
+🎋 Joke : ${response.data.setup}
+    `.trim())
 }
+
 handler.help = ['darkjoke']
-handler.tags = ['internet']
-handler.command = /^((drag|dark)joke)$/i
+handler.tags = ['fun']
+handler.command = /^(cuaca|weather)$/i
 
 module.exports = handler
