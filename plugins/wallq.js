36efa1d5436c184aef57c88a2bb05a1f8755e1d8
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-17 15:55:14 +0530

    Update wallq.js

--- plugins/wallq.js
+++ plugins/wallq.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch')
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `Pengunaan:\n${usedPrefix + command} <teks>\n\nContoh:\n${usedPrefix + command} Gunung kembar`
+  if (!text) throw `Usw:\n${usedPrefix + command} <text>\n\nExample:\n${usedPrefix + command} Twin mountains`
   let res = await fetch(global.API('https://wall.alphacoders.com/api2.0', '/get.php', {
     auth: '3e7756c85df54b78f934a284c11abe4e',
     method: 'search',
@@ -9,11 +9,11 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   }))
   if (!res.ok) throw eror
   let json = await res.json()
-  if (json.total_match = '0') throw `Tidak ditemukan!`
+  if (json.total_match = '0') throw `Not found!`
   let img = json.wallpapers[Math.floor(Math.random() * json.wallpapers.length)]
-  await conn.sendFile(m.chat, img.url_image, '', '© stikerin', m, 0, { thumbnail: Buffer.alloc(0) })
+  await conn.sendFile(m.chat, img.url_image, '', '© Alice 🥀', m, 0, { thumbnail: Buffer.alloc(0) })
 }
-handler.help = ['wallpaperq <pencarian>']
+handler.help = ['wallpaperq <search>']
 handler.tags = ['internet']
 handler.command = /^wall(paper)?q?$/i
 
