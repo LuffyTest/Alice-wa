8f63b2d21dc484f5b8b7e6fb2eb184885f938c46
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-17 17:02:06 +0530

    Update unregister.js

--- plugins/unregister.js
+++ plugins/unregister.js
@@ -1,10 +1,10 @@
 const { createHash } = require('crypto')
 
 let handler = async function (m, { args }) {
-  if (!args[0]) throw 'Serial Number nya mana?'
+  if (!args[0]) throw 'Wheres the serial number?'
   let user = db.data.users[m.sender]
   let sn = createHash('md5').update(m.sender).digest('hex')
-  if (args[0] !== sn) throw 'Serial Number salah'
+  if (args[0] !== sn) throw 'Wrong serial number'
   user.registered = false
   m.reply(`Unreg berhasil!`)
 }
