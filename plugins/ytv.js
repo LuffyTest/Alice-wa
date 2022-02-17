e01796f0f76c6109114bd5a350e658b541cf772a
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-17 10:55:08 +0530

    Update ytv.js

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -3,28 +3,28 @@ let fetch = require('node-fetch')
 const { servers, ytv } = require('../lib/y2mate')
 
 let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
-  if (!args || !args[0]) throw `Use:\n${usedPrefix + command} <url>\n\nEx:\n${usedPrefix + command} https://www.youtube.com/watch?v=RG3TpSGKMIA`
+  if (!args || !args[0]) throw `Use:\n${usedPrefix + command} <url>\n\nExample:\n${usedPrefix + command} https://www.youtube.com/watch?v=RG3TpSGKMIA&t=4s`
   let chat = db.data.chats[m.chat]
   let server = (args[1] || servers[0]).toLowerCase()
   try {
     let { dl_link, thumb, title, filesize, filesizeF } = await ytv(args[0], servers.includes(server) ? server : servers[0])
     let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-    m.reply(isLimit ? : `File Size${filesizeF}\nFile size above ${limit} MB, download it yourself..🥱: ${dl_link}` : wait)
+    m.reply(isLimit ? `File Size: ${filesizeF}\nFile size above ${limit} MB, download it yourself..🥱: ${dl_link}` : wait)
     let _thumb = {}
     try { _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
     catch (e) { }
     if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp4', `
 *🔮 Title:* ${title}
-*🔻 Video File Size:* ${filesizeF}
+*🔖 File Size:* ${filesizeF}
   `.trim(), m, 0, {
       ..._thumb,
       asDocument: chat.useDocument
     })
   } catch (e) {
-    return conn.sendButton(m.chat, 'Server Error', '', 'Try again', `${usedPrefix + command} ${args[0]}`)
+    return conn.sendButton(m.chat, 'Server Error', '', 'Try Again', `${usedPrefix + command} ${args[0]}`)
   }
 }
-handler.help = ['mp4', 'v', ''].map(v => 'yt' + v + ` <url>`)
+handler.help = ['mp4', 'v', ''].map(v => 'yt' + v + ` <url> [server: ${servers.join(', ')}]`)
 handler.tags = ['downloader']
 handler.command = /^yt(v|mp4)?$/i
 
