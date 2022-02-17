b09c645c6d0bf94cb25a2afe3321a34c45485583
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-17 18:33:12 +0530

    Update setbotbio.js

--- plugins/setbotbio.js
+++ plugins/setbotbio.js
@@ -1,6 +1,6 @@
 // NurNurz
 let handler = async (m, { conn, text }) => {
-  if (!text) throw `Use:\n${usedPrefix + command} <teks>\n\nExample:\n${usedPrefix + command} tes`
+  if (!text) throw `Use:\n${usedPrefix + command} <text>\n\nExample:\n${usedPrefix + command} tes`
   try {
     await conn.setStatus(text)
     m.reply('Succeed!')
