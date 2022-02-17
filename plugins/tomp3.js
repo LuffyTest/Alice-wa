10ba16f2127c1357cb8fd5810a0cb41687db6928
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-17 17:24:20 +0530

    Update tomp3.js

--- plugins/tomp3.js
+++ plugins/tomp3.js
@@ -4,13 +4,13 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   let q = m.quoted ? m.quoted : m
   let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
   if (/mp3|a(udio)?$/i.test(command)) {
-    if (!/video|audio/.test(mime)) throw `Balas audio dengan perintah *${usedPrefix + command}*`
+    if (!/video|audio/.test(mime)) throw `Reply audio with command *${usedPrefix + command}*`
     let media = await q.download()
     let audio = await toAudio(media, 'mp4')
     conn.sendFile(m.chat, audio, '', '', m, 0, { mimetype: 'audio/mp4', asDocument: db.data.chats[m.chat].useDocument })
   }
   if (/vn|ptt$/i.test(command)) {
-    if (!/video|audio/.test(mime)) throw `Balas audio dengan perintah *${usedPrefix + command}*`
+    if (!/video|audio/.test(mime)) throw `Reply audio with command *${usedPrefix + command}*`
     let media = await q.download()
     let audio = await toPTT(media, 'mp4')
     conn.sendFile(m.chat, audio, '', '', m, 1, { mimetype: 'audio/mp4' })
