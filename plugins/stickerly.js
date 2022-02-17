a45172c82f88c2b9e389c656824942036ee7b64f
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-17 18:05:40 +0530

    Update stickerly.js

--- plugins/stickerly.js
+++ plugins/stickerly.js
@@ -2,14 +2,14 @@ const fetch = require('node-fetch')
 const { sticker5 } = require('../lib/sticker')
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `Penggunaan:\n${usedPrefix + command} <teks>\n\nContoh:\n${usedPrefix + command} spongebob`
+    if (!text) throw `Use:\n${usedPrefix + command} <text>\n\nExample:\n${usedPrefix + command} spongebob`
     let res = await fetch(API('zeks', '/api/searchsticker', { q: text }, 'apikey'))
     if (!res.ok) throw eror
     let json = await res.json()
     if (!json.status) throw json
     let hasil = json.sticker.map((v, i) => `${i + 1}. ${v}`).join('\n')
     m.reply(`*${json.title}*
-*Estimasi selesai:* ${json.sticker.length * 1.5} detik
+*Estimated complete:* ${json.sticker.length * 1.5} detik
 `.trim())
 
     for (let i of json.sticker) {
@@ -20,7 +20,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     m.reply('_*Selesai*_')
 
 }
-handler.help = ['stikerly <teks>']
+handler.help = ['stikerly <text>']
 handler.tags = ['sticker']
 handler.command = /^(stic?kerly)$/i
 
