94db84b95b2e60f9578c38c6ff86ae7f2a47eb00
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-17 18:34:35 +0530

    Update ssweb.js

--- plugins/ssweb.js
+++ plugins/ssweb.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 
 let handler = async (m, { conn, command, args, usedPrefix }) => {
   let full = /f$/i.test(command)
-  if (!args[0]) throw `Pengunaan:\n${usedPrefix + command} <url>\n\nContoh:\n${usedPrefix + command} https://google.com/`
+  if (!args[0]) throw `Use:\n${usedPrefix + command} <url>\n\nExample:\n${usedPrefix + command} https://shokugekinosoma.fandom.com/wiki/Alice_Nakiri`
   let url = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
   let ss = await (await fetch(API('nrtm', '/api/ssweb', { delay: 1000, url, full }))).buffer()
   conn.sendFile(m.chat, ss, 'screenshot.png', url, m, 0, { thumbnail: ss })
