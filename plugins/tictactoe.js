663271d662c17e73b822b7b791fd585a9f06433c
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-17 17:38:20 +0530

    Update tictactoe.js

--- plugins/tictactoe.js
+++ plugins/tictactoe.js
@@ -4,13 +4,13 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
     conn.game = conn.game ? conn.game : {}
     if (/leave|exit|keluar/i.test(command)) {
         delete conn.game[Object.values(conn.game).find(room => room.state === 'WAITING').id]
-        throw '_Berhasil keluar dari permainan_'
+        throw '_Successfully exit the game_'
     } else {
         if (Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))) return conn.sendButton(m.chat, '_Kamu masih didalam game_', '', 'Keluar', ',tttexit', m)
         let room = Object.values(conn.game).find(room => room.state === 'WAITING' && (text ? room.name === text : true))
         // m.reply('[WIP Feature]')
         if (room) {
-            m.reply('_Partner ditemukan!_')
+            m.reply('_Partner found!_')
             room.o = m.chat
             room.game.playerO = m.sender
             room.state = 'PLAYING'
@@ -35,11 +35,11 @@ ${arr.slice(0, 3).join('')}
 ${arr.slice(3, 6).join('')}
 ${arr.slice(6).join('')}
 
-Menunggu @${room.game.currentTurn.split('@')[0]}
-Ketik *nyerah* untuk nyerah
+Waiting @${room.game.currentTurn.split('@')[0]}
+Type *give up* to give up
 `.trim()
-            if (room.x !== room.o) await conn.sendButton(room.x, str, '© stikerin', 'Nyerah', 'nyerah', m)
-            await conn.sendButton(room.o, str, '© stikerin', 'Nyerah', 'nyerah', m)
+            if (room.x !== room.o) await conn.sendButton(room.x, str, '© Alice 🥀', 'Give up', 'nyerah', m)
+            await conn.sendButton(room.o, str, '© Alice 🥀', 'Give up', 'nyerah', m)
         } else {
             room = {
                 id: 'tictactoe-' + (+new Date),
@@ -49,13 +49,13 @@ Ketik *nyerah* untuk nyerah
                 state: 'WAITING'
             }
             if (text) room.name = text
-            m.reply('_Menunggu partner_' + (text ? ` _mengetik command dibawah ini_
+            m.reply('_waiting for partner_' + (text ? ` _mengetik command dibawah ini_
 ${usedPrefix}${command} ${text}` : ''))
             conn.game[room.id] = room
         }
     }
 }
-handler.help = ['tictactoe', 'ttt', 'tttexit'].map(v => v + ' [nama room]')
+handler.help = ['tictactoe', 'ttt', 'tttexit'].map(v => v + ' [room name]')
 handler.tags = ['game']
 handler.command = /^(tictactoe|t{3})(exit)?$/
 
