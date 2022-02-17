78b5848bb543a1e8cb10ab85399546d8775430ba
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-17 16:04:37 +0530

    Update waifu.js

--- plugins/waifu.js
+++ plugins/waifu.js
@@ -4,10 +4,10 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   let res = await fetch(API('waifupics', '/sfw/waifu'))
   if (!res.ok) throw eror
   let json = await res.json()
-  conn.sendButtonImg(m.chat, json.url, 'Waifu', '© Rias Gremory 🎋', '🎐 Waifu 🎐', usedPrefix + command, m)
+  conn.sendButtonImg(m.chat, json.url, 'Waifu', '© Alice 🥀', '🎐 Waifu 🎐', usedPrefix + command, m)
 }
 handler.help = ['waifu']
-handler.tags = ['internet']
+handler.tags = ['anime']
 handler.command = /^(waifu)$/i
 
 module.exports = handler
