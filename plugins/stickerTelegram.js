c1151c99b16695afaa5f5714b3c67ecd14a1f8f4
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-17 18:26:03 +0530

    Update stickerTelegram.js

--- plugins/stickerTelegram.js
+++ plugins/stickerTelegram.js
@@ -2,14 +2,14 @@ const fetch = require('node-fetch')
 const { sticker5 } = require('../lib/sticker')
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Pengunaan:\n${usedPrefix + command} <url>\n\nContoh:\n${usedPrefix + command} https://t.me/addstickers/namapack`
+    if (!args[0]) throw `Use:\n${usedPrefix + command} <url>\n\nExample:\n${usedPrefix + command} https://t.me/addstickers/AliceNakiri_WABOT`
     if (!args[0].match(/(https:\/\/t.me\/addstickers\/)/gi)) throw `url salah`
     let packName = args[0].replace('https://t.me/addstickers/', '')
     let res = await fetch(`https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getStickerSet?name=${encodeURIComponent(packName)}`, { method: 'GET', headers: { 'User-Agent': 'GoogleBot' } })
     if (!res.ok) throw eror
     let json = await res.json()
     m.reply(`*Total stiker:* ${json.result.stickers.length}
-*Estimasi selesai:* ${json.result.stickers.length * 1.5} detik`.trim())
+*Estimated complete:* ${json.result.stickers.length * 1.5} seconds`.trim())
     for (let i = 0; i < json.result.stickers.length; i++) {
         let fileId = json.result.stickers[i].thumb.file_id
         let res = await fetch(`https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getFile?file_id=${fileId}`)
@@ -18,9 +18,9 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
         await conn.sendFile(m.chat, stiker, '', '', 0, 0, { asSticker: true })
         await conn.delay(1500)
     }
-    m.reply('_*Selesai*_')
+    m.reply('_Finished 🥀_')
 }
-handler.help = ['stikertele <url>']
+handler.help = ['stickertelegram <url>']
 handler.tags = ['sticker']
 handler.command = /^(stic?kertele(gram)?)$/i
 
