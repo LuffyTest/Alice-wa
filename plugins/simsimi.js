c472bf6906221701fc02be682df7b5f860db1a95
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-17 13:23:48 +0530

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,13 +1,13 @@
 let fetch = require('node-fetch')
 
 let handler = async (m, { text, usedPrefix, command }) => {
-  if (!text) throw `Penggunaan:\n${usedPrefix + command} <teks>\n\nContoh:\n${usedPrefix + command} hai`
+  if (!text) throw `Use:\n${usedPrefix + command} <text>\n\nExample:\n${usedPrefix + command} hi`
   let res = await fetch(API('https://api.simsimi.net', '/v2/', { text, lc: id }))
   if (!res.ok) throw eror
   let json = await res.json()
   await m.reply(`*Simi:* ${json.success}`)
 }
-handler.help = ['simi', 'simsimi'].map(v => v + ' <teks>')
+handler.help = ['simi', 'simsimi'].map(v => v + ' <text>')
 handler.tags = ['fun']
 handler.command = /^((sim)?simi|simih)$/i
 
