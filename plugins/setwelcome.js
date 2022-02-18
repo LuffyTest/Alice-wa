d652f42f4ce53af0a7c2cfdff346bfd963d349ca
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-18 16:59:03 +0530

    Update setwelcome.js

--- plugins/setwelcome.js
+++ plugins/setwelcome.js
@@ -2,10 +2,10 @@ let handler = async (m, { conn, text, isOwner, usedPrefix, command }) => {
   if (text) {
     if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
     db.data.chats[m.chat].sWelcome = text
-    m.reply('Welcome berhasil diatur\n@user (Mention)\n@subject (Judul Grup)\n@desc (Deskripsi Grup)')
-  } else throw `Penggunaan:\n${usedPrefix + command} <teks>\n\nContoh:\n${usedPrefix + command} selamat datang @user digrup @subject\n\n@desc`
+    m.reply('Welcome successfully set \ n@user (Mention) \ n@subject (Group Title) \ n@desc (Group Description)')
+  } else throw `Usage: \ n $ {usedPrefix + command} <text> \ n \ nExample: \ n $ {usedPrefix + command} welcome @user grouped @subject \ n \ n @desc`
 }
-handler.help = ['setwelcome <teks>']
+handler.help = ['setwelcome <text>']
 handler.tags = ['owner', 'group']
 handler.command = /^setwelcome$/i
 
