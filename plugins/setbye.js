03615b48b9a64f716c5fd4fb5b42492ddecce1ea
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-18 17:05:33 +0530

    Update setbye.js

--- plugins/setbye.js
+++ plugins/setbye.js
@@ -2,10 +2,10 @@ let handler = async (m, { conn, text, isOwner, usedPrefix, command }) => {
   if (text) {
     if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
     db.data.chats[m.chat].sBye = text
-    m.reply('Bye berhasil diatur\n@user (Mention)')
-  } else throw `Penggunaan:\n${usedPrefix + command} <teks>\n\ncontoh:\n${usedPrefix + command} byebye @user`
+    m.reply('Bye successfully set \ n@user (Mention)')
+  } else throw `Use:\n${usedPrefix + command} <text>\n\nExample:\n${usedPrefix + command} byebye.. @user`
 }
-handler.help = ['setbye <teks>']
+handler.help = ['setbye <text>']
 handler.tags = ['owner', 'group']
 handler.command = /^setbye$/i
 
