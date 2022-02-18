1c38ae30e8480c35dac02c76dc5fe6b9854a933e
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-18 17:08:21 +0530

    Update setProfileGroup.js

--- plugins/setProfileGroup.js
+++ plugins/setProfileGroup.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || ''
-    if (!/image\/(jpe?g|png)/.test(mime)) throw `Media tidak didukung!`
+    if (!/image\/(jpe?g|png)/.test(mime)) throw `Media not supported!`
     if (/image/.test(mime)) {
         let img = await q.download()
         await conn.updateProfilePicture(m.chat, img)
-    } else throw `Balas gambar dengan perintah *${usedPrefix + command}*`
+    } else throw `Reply to pictures with commands *${usedPrefix + command}*`
 }
 handler.help = ['setpp']
 handler.tags = ['group']
