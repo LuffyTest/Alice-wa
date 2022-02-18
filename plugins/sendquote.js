1664aadbbf65a57ddcac48e6b107720a27923f9d
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-18 17:10:28 +0530

    Update sendquote.js

--- plugins/sendquote.js
+++ plugins/sendquote.js
@@ -1,7 +1,7 @@
 async function handler(m) {
-    if (!m.quoted) throw `Balas pesan dengan perintah *${usedPrefix + command}*`
+    if (!m.quoted) throw `Reply to the message with a command *${usedPrefix + command}*`
     let q = this.serializeM(await m.getQuotedObj())
-    if (!q.quoted) throw 'Pesan yang anda balas tidak mengandung balasan!'
+    if (!q.quoted) throw 'The message you replied contains no replies!'
     await q.quoted.copyNForward(m.chat, true)
 }
 handler.help = ['q']
