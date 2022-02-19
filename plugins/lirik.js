ab17ecface97e184680af32d52eb722fa2b70348
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-19 13:08:25 +0530

    Update lirik.js

--- plugins/lirik.js
+++ plugins/lirik.js
@@ -1,12 +1,12 @@
 let finder = require('lyrics-finder')
 let handler = async (m, { text, usedPrefix, command }) => {
-  if (!text) throw `uhm.. cari apa?\n\ncontoh:\n${usedPrefix + command} dandelions`
+  if (!text) throw `uhm.. what are you looking for?\n\nExample:\n${usedPrefix + command} Gangsta's Paradise`
   let res = await finder("", text)
   if (!res) throw eror
 
   m.reply(res)
 }
-handler.help = ['lirik'].map(v => v + ' <teks>')
+handler.help = ['lyrics'].map(v => v + ' <text>')
 handler.tags = ['internet']
 handler.command = /^(lirik|lyrics?)$/i
 
