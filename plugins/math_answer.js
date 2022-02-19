f1acc9ba85b8b215d5aeeb7386f7b2bd8f001b1e
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-19 11:42:41 +0530

    Update math_answer.js

--- plugins/math_answer.js
+++ plugins/math_answer.js
@@ -5,19 +5,19 @@ handler.before = async function (m) {
   // if (!m.quoted || m.quoted.sender != this.user.jid || !/^Berapa hasil dari/i.test(m.quoted.text)) return !0
   this.math = this.math ? this.math : {}
   try {
-    if (!(id in this.math) && /^Berapa hasil dari/i.test(m.quoted.text)) return m.reply('Soal itu telah berakhir')
+    if (!(id in this.math) && /^🎗 What is the result of/i.test(m.quoted.text)) return m.reply('The matter has ended')
     // if (m.quoted.id == this.math[id][0].id) {
     let math = JSON.parse(JSON.stringify(this.math[id][1]))
     if (m.text == math.result) {
       global.db.data.users[m.sender].exp += math.bonus
       clearTimeout(this.math[id][3])
       delete this.math[id]
-      await this.sendButton(m.chat, `*Benar!* +${math.bonus} XP`, '© stikerin', `${math.mode.toUpperCase()}`, `.math ${math.mode}`, m)
+      await this.sendButton(m.chat, `*🎉🎊 Right!* +${math.bonus} XP`, '© Alice 🥀', `${math.mode.toUpperCase()}`, `.math ${math.mode}`, m)
     } else {
       if (--this.math[id][2] == 0) {
         clearTimeout(this.math[id][3])
         delete this.math[id]
-        await this.sendButton(m.chat, `*Kesempatan habis!*\nJawaban: *${math.result}*`, '© stikerin', `${math.mode.toUpperCase()}`, `.math ${math.mode}`)
+        await this.sendButton(m.chat, `*Opportunity is up!*\nAnswers: *${math.result}*`, '© Alice 🥀', `${math.mode.toUpperCase()}`, `.math ${math.mode}`)
       } else m.reply(`*Jawaban Salah!*\nMasih ada ${this.math[id][2]} kesempatan`)
     }
   } catch (e) { return }
