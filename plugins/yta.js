4c3dce1c8dc1fc5819f4daf97ec3ac858d5ef132
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-19 12:38:45 +0530

    Update yta.js

--- plugins/yta.js
+++ plugins/yta.js
@@ -18,6 +18,6 @@ handler.help = ['mp3/yta'].map(v => 'yt' + v + ` <url>`)
 handler.tags = ['downloader']
 handler.command = /^yt(a|mp3)$/i
 
-handler.limit = 1
+handler.limit = false
 
 module.exports = handler 
