385d8114a7330ee6a0c6484322aa2bc93d5ba892
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-19 09:44:18 +0530

    Update paylimit.js

--- plugins/paylimit.js
+++ plugins/paylimit.js
@@ -1,11 +1,11 @@
 let pajak = 0.02
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    let fail = `perintah ini buat ngasih limit ke pengguna lain\n\ncontoh:\n${usedPrefix + command} @6285157336614 10\natau balas pesan doi dengan perintah: ${usedPrefix + command} 10`
+    let fail = `This command is to give limits to other users\n\nExample:\n${usedPrefix + command} @9725135960 10\or reply to your message with the command: ${usedPrefix + command} 10`
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
     else who = m.chat
     if (!who) {
-        conn.reply(m.chat, fail, m, { contextInfo: { mentionedJid: ['6285157336614@s.whatsapp.net'] } })
+        conn.reply(m.chat, fail, m, { contextInfo: { mentionedJid: ['9725135960@s.whatsapp.net'] } })
         throw false
     }
     if (typeof global.db.data.users[who] == "undefined") {
@@ -17,8 +17,8 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
             name: conn.getName(m.sender),
             age: -1,
             regTime: -1,
-            afk: -1,
-            afkReason: '',
+            Afk: -1,
+            AfkReason: '',
             banned: false,
             level: 0,
             call: 0,
@@ -29,24 +29,24 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     }
     let txt = text.replace('@' + who.split`@`[0], '').trim()
     if (!txt) {
-        conn.reply(m.chat, fail, m, { contextInfo: { mentionedJid: ['6285157336614@s.whatsapp.net'] } })
+        conn.reply(m.chat, fail, m, { contextInfo: { mentionedJid: ['9725135960@s.whatsapp.net'] } })
         throw false
     }
-    if (isNaN(txt)) throw 'Hanya angka'
+    if (isNaN(txt)) throw 'only numbers'
     let poin = parseInt(txt)
     let limit = poin
     let pjk = Math.ceil(poin * pajak)
     limit += pjk
-    if (limit < 1) throw 'minimal 1'
+    if (limit < 1) throw 'minimum 1'
     let users = global.db.data.users
-    if (limit > users[m.sender].limit) throw 'Limit tidak mencukupi untuk mentransfer, ada pajaknya juga'
+    if (limit > users[m.sender].limit) throw 'The limit is not enough to transfer, there is a tax as well'
     users[m.sender].limit -= limit
     users[who].limit += poin
 
     m.reply(`(${-poin} Limit) + (${-pjk} Limit (Pajak 2%)) = ( ${-limit} Limit)`)
     conn.fakeReply(m.chat, `+${poin} Limit`, who, m.text)
 }
-handler.help = ['paylimit @user <jumlah>']
+handler.help = ['paylimit @user <amount>']
 handler.tags = ['xp']
 handler.command = /^payl(imit)?$/
 
