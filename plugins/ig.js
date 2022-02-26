702a01bd673d4aa94487b99bb73566222e2f1300
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-26 11:21:38 +0530

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -2,22 +2,23 @@ const { igdl } = require('../lib/scrape')
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-  if (!args[0]) throw `Pengunaan:\n${usedPrefix + command} <url>\n\nContoh:\n${usedPrefix + command} https://www.instagram.com/p/CQU21b0JKwq/`
-  if (!args[0].match(/https:\/\/www.instagram.com\/.*(p|reel|tv)/gi)) throw `url salah, perintah ini untuk mengunduh post/reel/tv`
+  if (!args[0]) throw `uhm.. where is the url?\n\nExample:\n${usedPrefix + command} https://www.instagram.com/`
+  if (!args[0].match(/https:\/\/www.instagram.com\/(p|reel|tv)/gi)) throw `wrong url, this command to download post/reel/tv`
 
   igdl(args[0]).then(async res => {
     let igdl = JSON.stringify(res)
     let json = JSON.parse(igdl)
     await m.reply(global.wait)
     for (let { downloadUrl, type } of json) {
-      conn.sendFile(m.chat, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), '© stikerin', m)
+      conn.sendFile(m.chat, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), Alice 🥀, m)
     }
   })
+
 }
 handler.help = ['ig'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(ig|instagram)$/i
+handler.command = /^(ig)$/i
 
-handler.limit = 1
+handler.limit = true
 
 module.exports = handler
