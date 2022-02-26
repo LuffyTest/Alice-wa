b77cbf70fec5f3a78e37cce0818de915cdbd035a
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-26 11:02:47 +0530

    Update img2braille.js

--- plugins/img2braille.js
+++ plugins/img2braille.js
@@ -6,7 +6,7 @@ const tmp = join(__dirname, '../tmp')
 async function handler(m, { usedPrefix, command }) {
 
     let q = m.quoted ? m.quoted : m
-    if (!/^image/.test(q.mimetype)) throw `Balas gambar dengan perintah *${usedPrefix + command}*`
+    if (!/^image/.test(q.mimetype)) throw `Reply image with command *${usedPrefix + command}*`
     let filename = join(tmp, + new Date + '.png')
     await fs.writeFile(filename, await q.download())
     m.reply(await braillefy(filename, 30, {
