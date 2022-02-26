352ed0dafd9e746e82ecebc23164afa6e15839de
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-26 10:51:57 +0530

    Update jodoh.js

--- plugins/jodoh.js
+++ plugins/jodoh.js
@@ -1,9 +1,9 @@
 const fetch = require('node-fetch')
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `contoh:\n${usedPrefix + command} ariffb|amel`
+    if (!text) throw `Example:\n${usedPrefix + command} Alice|Nakiri`
     let [nama1, nama2] = text.split(/[&|.]/i)
-    if (!nama1 || !nama2) throw `contoh:\n${usedPrefix + command} ariffb|amel`
+    if (!nama1 || !nama2) throw `Example:\n${usedPrefix + command} Alice|Nakiri`
 
     let res = await fetch(global.API('zeks', '/api/primbonjodoh', { nama1, nama2 }, 'apikey'))
     if (!res.ok) throw eror
@@ -11,18 +11,18 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!json.status) throw json
     let { thumb, positif, negatif } = json.result
     let caption = `
-*Nama kamu:* ${json.result.nama1}
-*Nama doi:* ${json.result.nama2}
+*Your name:* ${json.result.nama1}
+*doi's name:* ${json.result.nama2}
 
-*Positif:*
+*Positive:*
 ${positif}
 
-*Negatif:*
+*Negative:*
 ${negatif}
 `.trim()
     conn.sendFile(m.chat, thumb, 'file.png', caption, m, 0, { thumbnail: await (await fetch(thumb)).buffer() })
 }
-handler.help = ['jodoh'].map(v => v + ' <nama>|<nama doi>')
+handler.help = ['jodoh'].map(v => v + ' <nae>|<doi name>')
 handler.tags = ['fun']
 handler.command = /^(jodoh)$/i
 
