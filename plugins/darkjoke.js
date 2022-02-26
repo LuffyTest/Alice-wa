da43147ef439e5b873051327add843e0970174d4
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-26 10:30:08 +0530

    Update darkjoke.js

--- plugins/darkjoke.js
+++ plugins/darkjoke.js
@@ -4,12 +4,12 @@ let handler = async (m, { }) => {
   let res = await fetch(API('https://v2.jokeapi.dev/', '/joke/dark', {}, ''))
   if (!res.ok) throw `${res.status} ${res.statusText}`
   let json = await res.json()
-  m.reply(json.dark)
+  m.reply(`
 
 🎗 Category: ${json.category}
-🎋 Joke : ${json.setup}
+🎋 Joke: ${json.setup}
 🧨 delivery: ${json.delivery}
-
+ `.trim())
 }
 
 
