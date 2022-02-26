30206824abfdd59ac8659166e5683e4d7fc9adf6
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-26 10:04:54 +0530

    #shell

--- plugins/kapankah2.js
+++ plugins/kapankah2.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn, command, text }) => {
   m.reply(`
-*Pertanyaan:* ${command} ${text}?
-*Jawaban:* ${Math.floor(Math.random() * 10)} ${conn.pickRandom(['detik', 'menit', 'jam', 'hari', 'minggu', 'bulan', 'tahun', 'dekade', 'abad'])} lagi ...
+*Use:* ${command} ${text}?
+*Answer:* ${Math.floor(Math.random() * 10)} ${conn.pickRandom(['second', 'minute', 'hour', 'day', 'week', 'month', 'year', 'decade', 'century'])} again ...
 `.trim())
 }
-handler.help = ['', 'kah'].map(v => 'kapan' + v + ' <pertanyaan>')
+handler.help = ['', 'kah'].map(v => 'kapan' + v + ' <question>')
 handler.tags = ['kerang']
 handler.command = /^kapan(kah)?$/i
 
