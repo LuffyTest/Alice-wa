cebde98f3b5f39639dfbbdd984318fe99dc94b41
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-27 09:17:44 +0530

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -28,7 +28,7 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
 *🔻 Audio File Size:* ${filesizeF}
 *🔻 Video File Size:* ${yt2.filesizeF}
 *🍃 Server:* ${usedServer}
-`.trim(), '© Alice 🤍🥀', 'Audio', `.yta ${vid.url}`, 'Video', `.yt ${vid.url}`)
+`.trim(), '© Alice 🤍🥀', 'Audio', `.yta ${vid.url}`, 'Video', `.ytv ${vid.url}`)
 }
 handler.help = ['song','play','?','yt'].map(v => v + ' <query>')
 handler.tags = ['downloader']
