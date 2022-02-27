d67ddf08aec3c2f70e3ec11531d53fb14825b196
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-27 14:21:22 +0530

    Update addmsg.js

--- plugins/addmsg.js
+++ plugins/addmsg.js
@@ -1,22 +1,19 @@
 let { WAMessageProto } = require('@adiwajshing/baileys')
-
-let handler = async (m, { conn, command, usedPrefix, text }) => {
+let handler = async (m, { command, usedPrefix, text }) => {
     let M = WAMessageProto.WebMessageInfo
-    let which = command.replace(/add/i, '')
-    if (!m.quoted) throw `Balas pesan dengan perintah *${usedPrefix + command}*`
-    if (!text) throw `Pengunaan:${usedPrefix + command} <teks>\n\nContoh:\n${usedPrefix + command} tes`
-    let msgs = db.data.msgs
-    if (text in msgs) throw `'${text}' telah terdaftar!`
+    let which = command.replace(/\+|add/i, '')
+    if (!m.quoted) throw 'Reply Message!'
+    if (!text) throw `Use *${usedPrefix}list${which}* to see the list`
+    let msgs = global.db.data.msgs
+    if (text in msgs) throw `'${text}' registered in the message list`
     msgs[text] = M.fromObject(await m.getQuotedObj()).toJSON()
-    if (db.data.chats[m.chat].getmsg) return m.reply(`Berhasil menambahkan pesan '${text}'
+    m.reply(`Successfully added message in message list as '${text}'
     
-Akses dengan mengetik namanya`.trim())
-    else return await conn.sendButton(m.chat, `Berhasil menambahkan pesan '${text}'
-    
-akses dengan ${usedPrefix}get${which} ${text}`, '© stikerin', 'Aktifkan', '.on getmsg', m)
+Access with ${usedPrefix}get${which} ${text}
+or directly type the text`)
 }
-handler.help = ['vn', 'msg', 'video', 'audio', 'img', 'stiker', 'gif'].map(v => 'add' + v + ' <teks>')
+handler.help = ['vn', 'msg', 'video', 'gif', 'audio', 'img', 'sticker'].map(v => 'add' + v + ' <text>')
 handler.tags = ['database']
-handler.command = /^add(vn|msg|video|audio|img|stic?ker|gif)$/
+handler.command = /^(\+|add)(vn|msg|video|audio|img|stic?ker|gif)$/
 
 module.exports = handler
