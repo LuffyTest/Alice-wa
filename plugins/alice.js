723d10f10c8e4845d7a7f7252b532c3bb25452bb
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-27 09:55:05 +0530

    Update alice.js

--- plugins/alice.js
+++ plugins/alice.js
@@ -1,11 +1,7 @@
    
 let handler = async (m, { conn }) => {
  
-  conn.sendFile(m.chat, 'https://telegra.ph/file/c264ba134089f0b2ef129.mp4', '', '✨ Hello World ✨💞
-📊 Active Since: ${uptime}
- 🧬 Battery: ${conn.battery != undefined ? `${conn.battery.value}% ${conn.battery.live ? '🔌 Charging' : ''}` : 'Unknown'}
-
-Type .menu to get full menu 💞✨🤍', m)
+  conn.sendFile(m.chat, 'https://telegra.ph/file/c264ba134089f0b2ef129.mp4', '', '✨ Hello Honey Type *.menu*  to get Full menu. ✨🤍', m)
   
 }
 handler.help = ['alive']
