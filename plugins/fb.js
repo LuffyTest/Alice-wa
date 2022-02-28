a200ba479c692adf8d4d410f01f19f61e81f05f6
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-01 00:12:06 +0530

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -2,12 +2,12 @@ let fetch = require('node-fetch')
 
 let handler = async (m, { conn, command, text, usedPrefix }) => {
     if (!text) throw `uhm.. where is the url?\n\nUse:\n${usedPrefix + command} url\nExample :\n${usedPrefix + command} https://www.facebook.com/xxxxx/videos/xxxx`
-    let res = await fetch(API('xteam', '/dl/fbv2', { url: text }, 'apikey'))
+    let res = await fetch(API('neoxr', '/fb', { url: text }, 'apikey'))
     if (!res.ok) throw eror
     let json = await res.json()
     if (!json.status) throw json
     await m.reply(wait)
-    await conn.sendFile(m.chat, json.result.thumb, json.result.title, wm, m)
+    await conn.sendFile(m.chat, json.data.url, json.data.quality, wm, m)
 }
 handler.help = ['fb'].map(v => v + ' <url>')
 handler.tags = ['downloader']
