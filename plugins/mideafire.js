4aa51f25cd3b35f65bb118eafd5c53ee7ef40aec
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-02-28 23:36:16 +0530

    Update mideafire.js

--- plugins/mideafire.js
+++ plugins/mideafire.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 
 let handler = async (m, { conn, command, text, usedPrefix }) => {
     if (!text) throw `uhm.. where id the url?\n\nUse:\n${usedPrefix + command} url\nExample :\n${usedPrefix + command} http://www.mediafire.com/xxxx`
-    let res = await fetch(API('amel', '/mediafire', { url: text }, 'apikey'))
+    let res = await fetch(API('xteam', '/dl/mediafire', { url: text }, 'apikey'))
     if (!res.ok) throw eror
     let json = await res.json()
     if (!json.status) throw json
