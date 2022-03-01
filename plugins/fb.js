850d9eb482165d7c83ece5fb170187ce4d4f21b9
LuffyTest <98952894+LuffyTest@users.noreply.github.com>
2022-03-01 16:34:36 +0530

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -1,19 +1,25 @@
-const { facebook } = require('../lib/facebook')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) throw `uhm.. Where is the url?\n\nExample:\n${usedPrefix + command} https://www.facebook.com/alanwalkermusic/videos/277641643524720`
-  if (!args[0].match(/https:\/\/.*(facebook.com|fb.watch)/gi)) throw `Url error`
-  facebook(args[0]).then(async res => {
-    let fb = JSON.stringify(res)
-    let json = JSON.parse(fb)
-    // m.reply(require('util').format(json))
-    if (!json.status) throw json
-    await m.reply(global.wait)
-    await conn.sendFile(m.chat, json.data[0].url, '', '© Alice 🥀', m)
-  }).catch(_ => _)
+let xfar = require('xfarr-api')
+
+let fetch = require('node-fetch')
+
+let handler = async (m, { conn, command, text }) => {
+
+    if (!text) throw 'ලින්ක් එක 😒'
+
+  let res = await xfar.Facebook(text)
+
+m.reply('*WAIT...*')
+
+conn.sendFile(m.chat,res.medias[1].url, 'fb.mp4', `Tittle: ${res.tittle}
+Link Video: ${res.url}
+`, m)
+
 }
-handler.help = ['fb'].map(v => v + ' <url>')
+
+handler.help = ['fb <url>', 'facebook <url>']
+
 handler.tags = ['downloader']
 
-handler.command = /^f((b|acebook)(dl|download)?(er)?)$/i
+handler.command = /^fb|facebook$/i
 
 module.exports = handler
